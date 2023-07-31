e073b19bdae09016d0cdb6a3f8fbe72542eb7745
“Fedora38 <mike@nowhere>
2023-07-31 14:27:57 +0200

    Updates

--- src/b_soln.cxx
+++ src/b_soln.cxx
@@ -34,18 +34,38 @@ int main(int argc, char **argv)
 {
 	//const long Foo = 25000000000000;
 	long seed, max_seed = 548;
-	long limit,m,i, perim;
+	long limit,m,i,a,b,c,perim;
 	const double phi = 1.61803398874989;
 	long g_triangles = 0, count;	// Global count, local count
 	// Initialise variables
 	const long max_perim = 1000000;
-	for(seed = 12; seed != 13; ++seed) {
+	for(seed = 2; seed != max_seed; ++seed) {
 		limit = (long)std::floor(seed * phi);
 		for(i = 1; i <= limit; ++i) {
 			a = seed * seed;
 			b = seed * (seed + i);
 			c = (seed + i) * (seed + i);
 			perim = (a + b + c);
-		}
-	}	
+			if(perim <= max_perim) {
+				//std::cout<<setw(8)<<i<<setw(8)<<a<<setw(8)<<b<<setw(8)<<c<<setw(8)<<perim<<std::endl;
+				// This is the base case for this value of i.
+				// Find multiples of the base case for perimeters <= max_perim
+				long count = 1;
+				g_triangles += 1;
+				m = 2;
+				do{
+					perim = m * (a+b+c);
+					if (perim <= max_perim) {
+						g_triangles += 1;
+						m += 1;
+					} else {
+						break;
+					}
+				}while(1);
+				//std::cout<<"Found a total of "<<count<<" triangles for this config."<<std::endl;
+				//std::cout<<std::endl;				
+			} // if perim
+		} // for i...
+	} // for seed
+	std::cout<<g_triangles<<std::endl;
 }
